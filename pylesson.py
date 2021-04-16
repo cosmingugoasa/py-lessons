6a899e2f8de44bd9fddcae9627e3188b1e046180
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2021-04-16 20:28:30 +0200

    added wav extraction from mkv and file speech recognition

--- pylesson.py
+++ pylesson.py
@@ -1,15 +1,50 @@
 import speech_recognition as sr 
 import pyttsx3
+import sys
+import subprocess
+import os
+import datetime
 
-instance = sr.Recognizer()
+FFMPEG_FILE_PATH = os.path.abspath("ffmpeg.exe")
+AUDIO_FILE_PATH = os.path.abspath(sys.argv[-1])
 
-with sr.Microphone() as mic:
-    instance.adjust_for_ambient_noise(mic, duration=.4)
-    print("** In ascolto ... **")
-    audio = instance.listen(mic)
-    print("** Elaborazione ... **")
+#print(os.path.abspath(AUDIO_FILE_PATH))
+#os.system(f"{FFMPEG_FILE_PATH} -i {AUDIO_FILE_PATH} -ab 160k -ac 2 -ar 44100 -vn audio.wav")
+task = subprocess.run(f"{FFMPEG_FILE_PATH} -i {AUDIO_FILE_PATH} -ab 160k -ac 2 -ar 44100 -vn audio.wav", shell=True)
 
-    text = instance.recognize_google(audio, language="it-IT")
-    text = text.lower()
+if(task.returncode == 0):
+    print("** Audio file extracted succesfully **")
+    #print(os.path.abspath("audio.wav"))
 
-    print(f"Result : \n {text}")
+    instance = sr.Recognizer()
+    audioFile = sr.AudioFile("test_audio.wav")
+
+    try:
+        with audioFile as source:
+            instance.pause_threshold = 5.0
+            print("** In ascolto ... **")
+            audio = instance.listen(source)
+            print("** Elaborazione ... **")
+
+            text = instance.recognize_google(audio, language="it-IT")
+            text = text.lower()
+
+            print(f"Result : \n {text}")
+
+            #get date string to add in output file name
+            dateString = datetime.datetime.now()
+            dateString = str(dateString)[:10]
+            
+            #creating output file
+            output = open(f"lezione-{dateString.split(' ')[0]}.txt", "a")
+            output.write(text)
+            output.close()
+
+    except Exception as e:
+        print(e)
+
+else:
+    print("** Couldn't extract audio file **")
+
+
+
