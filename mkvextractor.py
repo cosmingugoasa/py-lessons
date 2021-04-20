d39999d0fff0da662fd17461e87364797b136ece
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2021-04-20 12:45:23 +0200

    added mkv args and basic destreamer integration

--- mkvextractor.py
+++ mkvextractor.py
@@ -2,15 +2,30 @@ import os
 import subprocess
 import sys
 import shutil
+import argparse
 
-DESTREAM_FOLDER_PATH = os.path.abspath("D:\Documents\Destream\destreamer")
-DESTREAM_SH = "./destreamer.sh"
-MKV_FILES_PATH = os.path.abspath("D:\Documents\Destream\destreamer\videos")
-studentMail = "cosmindaxgugoasa@gmail.com"
-TXT_LINKS_PATH = os.path.abspath("links.txt")
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--link", type=str, metavar=" ", help="link to the lesson")
+parser.add_argument("-f", "--file", type=str, metavar=" ", help="txt file with the links")
 
-#./destreamer.sh -f D:\Documents\Dev\py-lessons\links.txt  -u cosmin.gugoasa@studenti.unipr.it
-mkvExtractionTask = subprocess.run([f"cd {DESTREAM_FOLDER_PATH}", f"{DESTREAM_SH} -f {TXT_LINKS_PATH} -u {studentMail}"], shell=True)
+args = parser.parse_args()
+
+link = args.link
+txtFile = args.file
+
+DESTREAM_PATH = "D:\Documents\Destream\destreamer"
+STUDENT_MAIL = "cosmin.gugoasa@studenti.unipr.it"
+#link = "https://web.microsoftstream.com/video/ddcdf8ed-8046-4047-aeff-d93ca0a6da64?referrer=https:%2F%2Felly2020.dia.unipr.it%2F"
+
+os.chdir(DESTREAM_PATH)
+
+if(link != None):
+    mkvExtractionTask = subprocess.run(f"destreamer.cmd -u {STUDENT_MAIL} -i {link}", shell=True)
+elif(txtFile != None):
+        mkvExtractionTask = subprocess.run(f"destreamer.cmd -u {STUDENT_MAIL} -f {txtFile}", shell=True)
+else:
+    print("**No valid input. Run mkvextractor.py -h for help**")
+    sys.exit()
 
 if(mkvExtractionTask.returncode != 0):
     print("**Failed to download mkv file**")
